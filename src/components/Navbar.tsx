ccf18d33140e069b4feb6a83b28cd32df55a386e
ahmed <ahmedmijdo2@gmail.com>
2025-07-28 17:39:07 +0300

    enhance the rootlayout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ interface IProps {
 const Navbar = ({}: IProps) => {
     return (
         <nav className="my-5">
-            <ul className="uppercase flex flex-col md:flex-row space-y-5 justify-end items-center md:space-x-5">
+            <ul className="uppercase flex flex-col md:flex-row space-y-5 md:space-y-0 justify-end items-center md:space-x-5">
                 <li className="cursor-pointer"><NavLink to="products">Products</NavLink></li>
                 <li className="cursor-pointer"><NavLink to="solutions">Solutions</NavLink></li>
                 <li className="cursor-pointer"><NavLink to="pricing">Pricing</NavLink></li>
--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -9,13 +9,15 @@ interface IProps {
 const RootLayout = ({}: IProps) => {
     return (
         <>
-            <header className="flex flex-col justify-between items-center md:flex-row">
-                <div className="md:mx-2">
-                    CloudHost
-                </div>
-                <div className="flex flex-col items-center justify-evenly md:space-x-5 md:mx-2 md:flex-row">
-                    <Navbar />
-                    <Button className="bg-gray-600">Login</Button>
+            <header>
+                <div className="flex flex-col justify-between items-center md:flex-row">
+                    <div className="uppercase text-2xl bg-indigo-900 rounded-md w-full text-center md:text-start md:w-fit p-2">
+                        CloudHost
+                    </div>
+                    <div className="flex flex-col items-center justify-evenly md:space-x-5 md:mx-2 md:flex-row">
+                        <Navbar />
+                        <Button className="bg-gray-600">Login</Button>
+                    </div>
                 </div>
                 <Outlet/>
             </header>
