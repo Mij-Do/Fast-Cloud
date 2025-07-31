8dd411fded8d69ee81d46b4cfc392e59e08989aa
ahmed <ahmedmijdo2@gmail.com>
2025-07-31 16:00:39 +0300

    add Feature section

---
+++ src/components/FeatureCard.tsx
@@ -0,0 +1,21 @@
+import type { ReactNode } from "react";
+
+interface IProps {
+    children: ReactNode;
+    title: string;
+    description: string;
+}
+
+const FeatureCard = ({children, title, description}: IProps) => {
+    return (
+        <div className="bg-[#423D54] rounded-md flex flex-col space-y-3 p-2 w-full">
+            <div>
+                {children}
+            </div>
+            <h3>{title}</h3>
+            <p className="text-[#A69EB8]">{description}</p>
+        </div>
+    )
+}
+
+export default FeatureCard;
---
+++ src/components/KeyFeature.tsx
@@ -0,0 +1,35 @@
+import FeatureCard from "./FeatureCard";
+import { Headset } from 'lucide-react';
+import { Zap } from 'lucide-react';
+import { ShieldCheck } from 'lucide-react';
+
+interface IProps {
+
+}
+
+const KeyFeature = ({}: IProps) => {
+    return (
+        <div>
+            <h2 className="text-xl my-10">Key Features</h2>
+            <div className="flex space-x-3">
+                <FeatureCard 
+                    title="Speed" 
+                    description="Experience lightning-fast performance with our optimized infrastructure.">
+                    <Zap />
+                </FeatureCard>
+                <FeatureCard 
+                    title="Security" 
+                    description="Benefit from robust security measures to protect your data.">
+                    <ShieldCheck />
+                </FeatureCard>
+                <FeatureCard 
+                    title="Support" 
+                    description="Get 24/7 support from our expert team.">
+                    <Headset />
+                </FeatureCard>
+            </div>
+        </div>
+    )
+}
+
+export default KeyFeature;
